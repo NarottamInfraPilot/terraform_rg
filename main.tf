02295a7ae0c131c573219f042639b754b2d20ee6
Narottam <133843446+NarottamInfraPilot@users.noreply.github.com>
2025-10-16 20:44:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "4.48.0"
     }
   }
 backend "azurerm" {
-resource_group_name   = "Narottam-rg"
-    storage_account_name  = "mystorage836753"
-    container_name        = "mystorage836753"
-    key                    = "statefile"
+# resource_group_name   = "Narottam-rg"
+#    storage_account_name  = "mystorage836753"
+#    container_name        = "mystorage836753"
+#    key                    = "statefile"
 }
 }
 
 provider "azurerm" {
   features {}
+subscription_id = "cba3453a-ff1f-4f51-b6f6-5a1149843776"
 }
 resource "azurerm_resource_group" "rg" {
   name     = "prod-rg"
